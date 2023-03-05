9b059fd1b4d0a01a0f80def4eb23f24dcad85313
BenSmook <besmookler@gmail.com>
2023-03-05 12:14:15 -0800

    commits with kevins calculations, a second page added to streamlit ui, and the calculations added to the second page of the streamlit ui
    Co-Authored-By: Kevin Herndon  <114819378+Kevtech577@users.noreply.github.com>

--- real_app.py
+++ real_app.py
@@ -77,7 +77,7 @@ API_KEY = os.getenv('X_RapidAPI_Key')
 API_HOST = os.getenv('X_RapidAPI_Host')
 
 
-@st.cache
+@st.cache_data
 
 def get_property_listings(city, state, limit):
     url = "https://realty-mole-property-api.p.rapidapi.com/saleListings"
@@ -131,38 +131,117 @@ def fit_knn(real_estate_df, address, relevant_features=["latitude", "longitude",
 
 
 
+def page1():
+    st.title("Real Estate Listings")
+    st.sidebar.header("Search Listings")
 
-st.title("Real Estate Listings")
-st.sidebar.header("Search Listings")
+    state_options = list(state_city_dict.keys())
+    default_state = state_options[0]
+    city_options = state_city_dict[default_state]
+    default_city = city_options[0]
 
-state_options = list(state_city_dict.keys())
-default_state = state_options[0]
-city_options = state_city_dict[default_state]
-default_city = city_options[0]
+    selected_state = st.sidebar.selectbox('Select a state', options=state_options, index=0)
+    selected_city = st.sidebar.selectbox('Select a city', options=state_city_dict[selected_state], index=0)
+    selected_limit = st.sidebar.selectbox('Select how many listings', options=[100, 150, 200], index=0)
 
-selected_state = st.sidebar.selectbox('Select a state', options=state_options, index=0)
-selected_city = st.sidebar.selectbox('Select a city', options=state_city_dict[selected_state], index=0)
-selected_limit = st.sidebar.selectbox('Select how many listings', options=[100, 150, 200], index=0)
+    st.sidebar.header("Search")
+    search_button = st.sidebar.button("Search")
 
-st.sidebar.header("Search")
-search_button = st.sidebar.button("Search")
-
-if search_button:
-    property_listings = get_property_listings(selected_city, selected_state, selected_limit)
-    st.write(property_listings)
+    if search_button:
+        property_listings = get_property_listings(selected_city, selected_state, selected_limit)
+        st.write(property_listings)
 
 
-real_estate_df = get_property_listings(selected_city, selected_state, selected_limit)
-st.write(real_estate_df)
-map_real_estate = create_map(real_estate_df)
+    real_estate_df = get_property_listings(selected_city, selected_state, selected_limit)
+    st.write(real_estate_df)
+    map_real_estate = create_map(real_estate_df)
 
-st.write("Map of Real Estate Properties")
-st_folium(map_real_estate)
+    st.write("Map of Real Estate Properties")
+    st_folium(map_real_estate)
 
-st.header("Actual Price Prediction Using KNN Neighbors")
+    st.header("Actual Price Prediction Using KNN Neighbors")
 
-address = st.text_input("Enter an address:")
-if address:
-    predicted_price = fit_knn(real_estate_df, address)
-    st.write(predicted_price)
+    address = st.text_input("Enter an address:")
+    if address:
+        predicted_price = fit_knn(real_estate_df, address)
+        st.write(predicted_price)
 
+def page2():       
+    with st.container():
+        st.header("Investor Calculators")
+        st.header("Mortgage Calculator and ROI Calculator")
+        st.write("Use the following calculators to estimate your mortgage payments and ROI:")
+
+
+        # Set the API endpoint and define the parameters
+        api_url = 'https://api.api-ninjas.com/v1/mortgagecalculator'
+        params = {
+            'loan_amount': st.number_input('Loan amount', value=200000),
+            'interest_rate': st.slider('Interest rate', min_value=0.0, max_value=10.0, step=0.1, value=3.5),
+            'duration_years': st.selectbox('Duration in years', options=[10, 15, 20, 25, 30, 35, 40], index=4),
+        }
+
+        # Set the API key in the request headers and handle the response
+        headers = {'X-Api-Key': 'knc8VXNlwMozZwpq6LFMWQ==FKgTtbuDkJuNXtSO'}
+        response = requests.get(api_url, headers=headers, params=params)
+        if response.status_code == requests.codes.ok:
+            result = response.json()
+            st.write("Mortgage Calculator Result:")
+            st.write("----------------------------")
+            st.write("Monthly Payment:")
+            st.write(f"   - Total: ${result['monthly_payment']['total']}")
+            st.write(f"   - Mortgage: ${result['monthly_payment']['mortgage']}")
+            st.write(f"   - Property Tax: ${result['monthly_payment']['property_tax']}")
+            st.write(f"   - HOA: ${result['monthly_payment']['hoa']}")
+            st.write(f"   - Annual Home Insurance: ${result['monthly_payment']['annual_home_ins']}")
+            st.write("Annual Payment:")
+            st.write(f"   - Total: ${result['annual_payment']['total']}")
+            st.write(f"   - Mortgage: ${result['annual_payment']['mortgage']}")
+            st.write(f"   - Property Tax: ${result['annual_payment']['property_tax']}")
+            st.write(f"   - HOA: ${result['annual_payment']['hoa']}")
+            st.write(f"   - Annual Home Insurance: ${result['annual_payment']['home_insurance']}")
+            st.write("Total Interest Paid: ${result['total_interest_paid']}")
+        else:
+            st.error(f"Error: {response.status_code} - {response.text}")
+
+        # Create sidebar inputs for property value and loan amount for each property
+        st.sidebar.header("LTV Calculator")
+        num_properties = st.sidebar.number_input("Number of Properties", value=1, min_value=1)
+        property_values = []
+        loan_amounts = []
+
+        for i in range(num_properties):
+            st.sidebar.subheader(f"Property {i+1}")
+            property_value = st.sidebar.number_input(f"Property {i+1} Value", value=1000000)
+            loan_amount = st.sidebar.number_input(f"Property {i+1} Loan Amount", value=800000)
+            property_values.append(property_value)
+            loan_amounts.append(loan_amount)
+
+        # Calculate LTV ratio for each property and display result
+        for i in range(num_properties):
+            ltv_ratio = np.round(loan_amounts[i] / property_values[i], 2) * 100
+            st.write(f"Property {i+1} LTV Ratio:", ltv_ratio, "%")
+
+# Create sidebar inputs for initial investment, cash flow, and holding period
+    st.sidebar.header("ROI Calculator")
+    initial_investment = st.sidebar.number_input("Initial Investment", value=100000)
+    cash_flow = st.sidebar.number_input("Cash Flow", value=10000)
+    holding_period = st.sidebar.number_input("Holding Period (years)", value=5)
+
+    # Calculate ROI and display result
+    total_return = initial_investment + cash_flow * holding_period
+    roi = np.round((total_return - initial_investment) / initial_investment, 2) * 100
+    st.write("ROI:", roi, "%")
+
+def app():
+    st.sidebar.title("Navigation")
+    page_options = ["Page 1", "Page 2"]
+    page_selection = st.sidebar.radio("Go to", page_options)
+
+    if page_selection == "Page 1":
+        page1()
+    elif page_selection == "Page 2":
+        page2()
+
+if __name__ == '__main__':
+    app()
